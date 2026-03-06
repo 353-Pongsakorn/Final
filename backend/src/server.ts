3a586dd4036e70aed03d418dde9ac1520575394c
353-Pongsakorn <pongsakorntananon868@gmail.com>
2026-03-06 13:13:20 +0700

    TH commit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'https://transcedent-boba-17c942.netlify.app', // วางลิงก์ Netlify ของคุณตรงนี้
+  origin: 'transcendent-boba-17c942.netlify.app', // วางลิงก์ Netlify ของคุณตรงนี้
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
